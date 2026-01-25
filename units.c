381e54c1fb6c5aea85b76668bb0dc5104837e25c
krypton67 <damienshawn67@gmail.com>
2026-01-25 16:17:43 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	//
+	printf("Units Offered This Semester:\n");
+	printf("1.Computational Logic\n");
+	printf("2.Communication Skills\n");
+	printf("3.Digital Literacy\n");
+	printf("4.Health And wellness Management\n");
+	printf("5.Structured Programming And Algorithms\n");
+	printf("6.Basic Mathematics\n");
+	printf("7.Introduction To Physics\n");
+	return 0;
+}	
---
+++ user_input.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+int main() {
+	//
+	float height;
+	double bankbalance;
+	char phoneNumber[20];//Using string to accomodate leading zeros or long numbers
+	printf("Enter Your Height(in meters or centimeters)");
+	scanf("%f", &height);
+	printf("Enter your bank balance(in kenya shillings)");
+	scanf("%lf",&bankbalance);
+	printf("Enter your phoneNumber:");
+	scanf("%s",phoneNumber);//%s handles string input
+	printf("\n---User Details---\n");
+	printf("Height:%2f\n",height);
+	printf("BankBalance:%2lf KES\n",bankbalance);
+	printf("PhoneNumber:%s\n",phoneNumber);
+	
+	return 0;
+} 
+	 
