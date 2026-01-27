61fc9012b693504a0a5e6dfd19cf7ba76d689e11
krypton67 <damienshawn67@gmail.com>
2026-01-27 20:53:01 +0300

    Update units.c

--- units.c
+++ units.c
@@ -1,6 +1,9 @@
+/*Program for the units offered this semester
+Name:Shawn mbithi
+Registration No.BCS-03-0019/2026
+*/ 
 #include<stdio.h>
 int main(){
-	//
 	printf("Units Offered This Semester:\n");
 	printf("1.Computational Logic\n");
 	printf("2.Communication Skills\n");
@@ -10,4 +13,5 @@ int main(){
 	printf("6.Basic Mathematics\n");
 	printf("7.Introduction To Physics\n");
 	return 0;
+
 }	
